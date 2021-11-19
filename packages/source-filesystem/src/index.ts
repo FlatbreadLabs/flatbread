222db674732b3edfa2f38adf521441ea6904c676
Tony Ketcham <ketcham@pnw.edu>
2021-11-19 01:35:22 -0600

    feat(source-filesystem): source factory

--- packages/source-filesystem/src/index.ts
+++ packages/source-filesystem/src/index.ts
@@ -9,18 +9,18 @@ import type { VFile } from 'vfile';
  * Get filenames from a directory of files that match
  * the expected file extensions.
  *
- * @param dir The directory to read from
+ * @param path The directory to read from
  * @param extensions File extensions to parse
  * @returns A list of content nodes' filenames
  */
-export async function getValidNodesFilenames(
-  dir: string,
+async function getValidNodesFilenames(
+  path: string,
   extensions?: string[]
 ): Promise<string[]> {
   // If no extensions are provided, use the default ones
   const validExtensions = extensions ?? ['.md', '.mdx', '.markdown'];
 
-  const files = await fs.readdir(join(process.cwd(), dir));
+  const files = await fs.readdir(join(process.cwd(), path));
   return files.filter((f) =>
     validExtensions.includes(extname(f).toLowerCase())
   );
@@ -29,15 +29,44 @@ export async function getValidNodesFilenames(
 /**
  * Get nodes (files) from the directory
  *
- * @param dir The directory to read from
+ * @param path The directory to read from
  * @returns An array of content nodes
  */
-export default async function getNodesFromDirectory(
-  dir: string
-): Promise<Promise<VFile>[]> {
-  const slugs: string[] = await getValidNodesFilenames(dir);
+async function getNodesFromDirectory(path: string): Promise<Promise<VFile>[]> {
+  const slugs: string[] = await getValidNodesFilenames(path);
 
   return slugs.map(
-    async (slug: string): Promise<VFile> => await read(join(dir, slug))
+    async (slug: string): Promise<VFile> => await read(join(path, slug))
   );
 }
+
+/**
+ * Returns all nodes from the directory
+ *
+ * @param paths array of directories to read from
+ * @returns
+ */
+function getAllNodes(
+  allContentTypes: Record<string, any>[]
+): Record<string, []> {
+  return Object.fromEntries(
+    allContentTypes.map((contentType) => [
+      contentType.typeName,
+      getNodesFromDirectory(contentType.path),
+    ])
+  );
+}
+
+/**
+ * Source filesystem plugin for fetching flat-file content nodes from directories on disk.
+ *
+ * @param config content types config
+ * @returns A function that returns functions which fetch lists of nodes
+ */
+const source = (config?: Record<string, any>) => ({
+  fetchByType: (path: string) => getNodesFromDirectory(path),
+  fetch: (allContentTypes: Record<string, any>[]) =>
+    getAllNodes(allContentTypes),
+});
+
+export default source;
