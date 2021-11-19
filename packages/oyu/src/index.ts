afcc0f423f1bfe146de1670ec3de2fc24f3bf826
Tony Ketcham <ketcham@pnw.edu>
2021-11-19 01:51:39 -0600

    fix(oyu): pave over deprecated API

--- packages/oyu/src/index.ts
+++ packages/oyu/src/index.ts
@@ -1,34 +1,34 @@
-import getNodesFromDirectory from '@oyu/source-filesystem';
-import { nodeToJSON } from '@oyu/transformer-markdown';
-// import github from 'remark-github';
+// import getNodesFromDirectory from '@oyu/source-filesystem';
+// import { nodeToJSON } from '@oyu/transformer-markdown';
+// // import github from 'remark-github';
 
-import type { TransformerConfig } from '@oyu/transformer-markdown';
-import type { VFile } from 'vfile';
+// import type { TransformerConfig } from '@oyu/transformer-markdown';
+// import type { VFile } from 'vfile';
 
-const nodes = await getNodesFromDirectory('content/authors');
+// const nodes = await getNodesFromDirectory('content/authors');
 
-const defaultConfig: TransformerConfig = {
-  markdown: {
-    gfm: true,
-    externalLinks: true,
-    // remarkPlugins: [github],
-  },
-};
+// const defaultConfig: TransformerConfig = {
+//   markdown: {
+//     gfm: true,
+//     externalLinks: true,
+//     // remarkPlugins: [github],
+//   },
+// };
 
-/**
- * Convert an iterable set of content to an array of objects
- *
- * @param nodes array of content files
- * @returns array of JSON-transformed content files
- */
-async function convertNodesToJSON(
-  nodes: Promise<VFile>[]
-): Promise<Record<any, any>> {
-  let data = [];
-  for (let node of nodes) {
-    data.push(await nodeToJSON(await node, defaultConfig));
-  }
-  return data;
-}
-const result = await convertNodesToJSON(nodes);
-console.log(result);
+// /**
+//  * Convert an iterable set of content to an array of objects
+//  *
+//  * @param nodes array of content files
+//  * @returns array of JSON-transformed content files
+//  */
+// async function convertNodesToJSON(
+//   nodes: Promise<VFile>[]
+// ): Promise<Record<any, any>> {
+//   let data = [];
+//   for (let node of nodes) {
+//     data.push(await nodeToJSON(await node, defaultConfig));
+//   }
+//   return data;
+// }
+// const result = await convertNodesToJSON(nodes);
+// console.log(result);
