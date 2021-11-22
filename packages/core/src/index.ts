4102859d987f8a39e858179771f43a48ecdaa7e6
Tony Ketcham <tonyketcham@gmail.com>
2021-11-22 15:38:32 -0600

    feat(core): begin schema generation flow

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,31 +1,66 @@
-// import { FlatbreadJsonNode } from './types';
+import { ConfigResult, FlatbreadConfig } from 'flatbread';
 
-// core logic will go here
 export * from './types';
 
-// const summarizeTypeShape = (nodes: FlatbreadJsonNode[]): Record<string, any> => {
-//   const schema = nodes.reduce((acc, node) => {
-//     Object.keys(arr);
-//   }, {});
-//   return;
-// };
+/**
+ * Generates a GraphQL schema from content nodes.
+ *
+ * @param configResult the result of the config file processing
+ */
+const generateSchema = async (configResult: ConfigResult<FlatbreadConfig>) => {
+  const { config } = configResult;
+  if (!config) {
+    throw new Error('Config is not defined');
+  }
 
-// import {schemaComposer} from 'graphql-compose';
+  // Invoke the content source resolver to retrieve the content nodes
+  const allContentNodes = await config.source.fetch(config.content);
 
-// const AuthorTC = schemaComposer.createObjectTC({
-//   name: 'Author',
-//   fields: {
-//     id: 'Int!',
-//     firstName: 'String',
-//     lastName: 'String',
-//     posts: {
-//       type: () => [PostTC], // arrow function for `type` helps to solve hoisting problems and keep ability to list all fields
-//       args: {
-//         limit: { type: 'Int', defaultValue: 20 },
-//         skip: 'Int', // shortand to `{ type: 'Int' }`
-//         sort: `enum AuthorPostsSortEnum { ASC DESC }`, // type creation via SDL
-//       },
-//       resolve: () => { ... },
-//     }
-//   },
-// });
+  // Transform the content nodes to the expected JSON format if needed
+  const allContentNodesJSON = optionallyTransformContentNodes(
+    allContentNodes,
+    config
+  );
+
+  console.log(JSON.stringify(allContentNodesJSON, null, 2));
+
+  // Create map of references on each content node
+  // ...
+
+  // Generate the schema and return it
+  // ...
+};
+
+/**
+ * Transforms the content nodes to the expected JSON format. If no transformer is defined, the content nodes are returned as is.
+ *
+ * @param allContentNodes an object of keys and values - content type and content nodes to transform, respectively
+ * @param config
+ * @returns
+ */
+const optionallyTransformContentNodes = (
+  allContentNodes: Record<string, any[]>,
+  config: FlatbreadConfig
+): Record<string, any[]> => {
+  if (config.transformer && config.transformer.parse) {
+    const parse = config.transformer.parse;
+
+    /**
+     * Map through each content type,
+     * then map through each content node
+     * and transform it with the provided parser
+     *
+     * @todo if this becomes a performance bottleneck, consider overloading the source plugin API to accept a transform function so we can avoid mapping through the content nodes twice
+     * */
+    const transformedContentNodes = Object.fromEntries(
+      Object.entries(allContentNodes).map(([contentType, contentNodes]) => [
+        contentType,
+        contentNodes.map((node) => parse(node)),
+      ])
+    );
+    return transformedContentNodes;
+  }
+  return allContentNodes;
+};
+
+export default generateSchema;
