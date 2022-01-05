d85e9593119d075abc436b76f8ccf2382dbc1aa8
tonyketcham <tony@karmalies.studio>
2022-01-05 00:15:50 -0600

    refactor(core): robust query argument generators

---
+++ packages/core/src/generators/arguments.ts
@@ -0,0 +1,100 @@
+/**
+ * Generates the accepted arguments for an 'All' query on a content type.
+ *
+ * @param pluralType plural name of the content type
+ */
+export const generateArgsForAllItemQuery = (pluralType: string) => ({
+  ...skip(),
+  ...limit(pluralType),
+  ...order(pluralType, 'ASC'),
+  ...sortBy(pluralType),
+  ...filter(pluralType),
+});
+
+/**
+ * Generates the accepted arguments for a 'many-item' query on a content type.
+ *
+ * @param pluralType plural name of the content type
+ */
+export const generateArgsForManyItemQuery = (pluralType: string) => ({
+  ids: {
+    type: '[String]',
+  },
+  ...skip(),
+  ...limit(pluralType),
+  ...order(pluralType, 'ASC'),
+  ...sortBy(pluralType),
+});
+
+/**
+ * Generates the accepted arguments for a 'single-item' query on a content type.
+ *
+ */
+export const generateArgsForSingleItemQuery = () => ({
+  id: {
+    type: 'String',
+  },
+});
+
+/**
+ * Argument for skipping the first `n` items from the query results.
+ */
+export const skip = () => ({
+  skip: {
+    description: 'Skip the first `n` results',
+    type: 'Int',
+  },
+});
+
+/**
+ * Argument for limiting the maximum number of items in the query results.
+ *
+ * @param pluralType plural name of the content type
+ */
+export const limit = (pluralType: string) => ({
+  limit: {
+    description: `The maximum number of ${pluralType} to return`,
+    type: 'Int',
+  },
+});
+
+/**
+ * Argument for ordering the direction of sorting items in the query results.
+ *
+ * @param pluralType plural name of the content type
+ * @param defaultOrder default order to use if not explicitly specified in the query
+ */
+export const order = (
+  pluralType: string,
+  defaultValue: 'ASC' | 'DESC' = 'ASC'
+) => ({
+  order: {
+    description: `Which order to return ${pluralType} in`,
+    type: `enum Order { ASC DESC }`,
+    defaultValue,
+  },
+});
+
+/**
+ * Argument for the field to sort items by in the query results.
+ *
+ * @param pluralType plural name of the content type
+ */
+export const sortBy = (pluralType: string) => ({
+  sortBy: {
+    description: `The field to sort ${pluralType} by`,
+    type: 'String',
+  },
+});
+
+/**
+ * Argument for the deep filter to apply to items in the query results.
+ *
+ * @param pluralType plural name of the content type
+ */
+export const filter = (pluralType: string) => ({
+  filter: {
+    description: `Filter ${pluralType} by a JSON object`,
+    type: 'JSON',
+  },
+});
