725489e1f195ac968b6a081eac0a6c03a8c50ce7
tonyketcham <tony@karmalies.studio>
2022-05-30 01:42:57 -0500

    update playground

--- playground/svelte.config.js
+++ playground/svelte.config.js
@@ -1,8 +1,6 @@
 import preprocess from 'svelte-preprocess';
-// import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { resolve } from 'path';
-// import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,8 +11,6 @@ const config = {
   // preprocess: [preprocess()],
 
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
     adapter: adapter(),
     vite: {
       resolve: {
@@ -28,30 +24,6 @@ const config = {
           ),
         },
       },
-      // define: {
-      //   __OYU_GQL_HOST__: ['http://localhost:1738'],
-      // },
-      // plugins: [
-      //   (function startingPlugin() {
-      //     return {
-      //       name: 'starty',
-      //       buildStart() {
-      //         process.env.HELLOOO = 'world';
-      //         console.log('hello world');
-      //         // do something with this list
-      //       },
-      //     };
-      //   })(),
-      //   (function endingPlugin() {
-      //     return {
-      //       name: 'endy',
-      //       buildStart() {
-      //         console.log(process.env.HELLOOO + ' is ending');
-      //         // do something with this list
-      //       },
-      //     };
-      //   })(),
-      // ],
     },
   },
 
