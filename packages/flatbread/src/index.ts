34caff56b89c427ee53f02fcfa866a525ed9837a
tonyketcham <tony@karmalies.studio>
2022-06-29 23:03:43 -0500

    improved interdependent package versioning ranges

--- packages/flatbread/src/index.ts
+++ packages/flatbread/src/index.ts
@@ -1,4 +1,8 @@
 export * from '@flatbread/config';
 export * from '@flatbread/core';
+
 export * from '@flatbread/source-filesystem';
+export { default as filesystem } from '@flatbread/source-filesystem';
+
 export * from '@flatbread/transformer-markdown';
+export { default as markdownTransforer } from '@flatbread/transformer-markdown';
