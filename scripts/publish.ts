edd25e964a62407d3470a37243f53f26c392e0f5
tonyketcham <tony@karmalies.studio>
2022-07-01 04:26:59 -0500

    I feel oh so silly

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -7,9 +7,7 @@ import getPackagesManifest from './utils/packageManifest';
 
 execSync('pnpm run build', { stdio: 'inherit' });
 
-let command = 'npm publish --access public';
-
-// if (version.includes('alpha')) command += ' --tag alpha';
+let command = 'pnpm publish --access public';
 if (version.includes('beta')) command += ' --tag beta';
 
 const dirs = await fs.readdir('packages');
