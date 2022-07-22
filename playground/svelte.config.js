d64913bcc046ce3f6dbe834ce3d1b5a374331f9d
tonyketcham <tony@karmalies.studio>
2022-07-21 23:47:10 -0500

    update SvelteKit

--- playground/svelte.config.js
+++ playground/svelte.config.js
@@ -1,32 +1,12 @@
 import preprocess from 'svelte-preprocess';
-// import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
-import { resolve } from 'path';
-// import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte'],
 
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  // preprocess: [preprocess()],
-
   kit: {
     adapter: adapter(),
-    vite: {
-      resolve: {
-        alias: {
-          flatbread: resolve('../packages/flatbread/src/index.ts'),
-          '@flatbread/transformer-markdown': resolve(
-            '../packages/transformer-markdown/src/index.ts'
-          ),
-          '@flatbread/source-filesystem': resolve(
-            '../packages/source-filesystem/src/index.ts'
-          ),
-        },
-      },
-    },
   },
 
   preprocess: [preprocess({})],
