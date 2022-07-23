37df7b2b6a88a6bfb86fadb4aa7856a5472a6b28
Tony Ketcham <tony@modyfi.io>
2022-07-23 08:49:21 -0500

    drop CJS support

--- packages/config/tsup.config.ts
+++ packages/config/tsup.config.ts
@@ -5,7 +5,7 @@ export const tsup: Options = {
   sourcemap: true,
   clean: true,
   entryPoints: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['esm'],
   target: 'esnext',
   dts: true,
   shims: true,
