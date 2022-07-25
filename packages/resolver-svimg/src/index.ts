8ab37511bc3dbaf0833c369b1e0ab4580e3e9ca5
Adam Sparks <adamsparks92@gmail.com>
2022-07-25 00:03:05 -0500

    made it simpler with JSON type too

--- packages/resolver-svimg/src/index.ts
+++ packages/resolver-svimg/src/index.ts
@@ -1,30 +1,8 @@
 import { Queue, generateComponentAttributes } from 'svimg/dist/process.js';
 
-const SVIMG_TYPE = `
-type Svimg {
-  srcset: String
-  srcsetwebp: String
-  srcsetavif: String
-  placeholder: String
-  aspectratio: Float
-}`;
-
 const queue = new Queue();
 
-// copied from https://github.com/xiphux/svimg/blob/master/src/component/generate-component-attributes.ts#L9
-interface Config {
-  src: string;
-  queue?: Queue;
-  inputDir: string;
-  outputDir: string;
-  publicPath?: string;
-  webp?: boolean;
-  avif?: boolean;
-  widths?: number[];
-  quality?: number;
-  skipGeneration?: boolean;
-  skipPlaceholder?: boolean;
-}
+type Config = Parameters<typeof generateComponentAttributes>[0];
 
 /**
  *
@@ -35,7 +13,7 @@ interface Config {
 export function createSvImgField(field: string, config: Omit<Config, 'src'>) {
   return {
     field,
-    type: SVIMG_TYPE,
+    type: 'JSON',
     resolve(src: string) {
       if (!src) return null;
       return generateComponentAttributes({ queue, ...config, src });
