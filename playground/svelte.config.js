21365a48d9dd774110dd49da63af6dab10b1511b
Adam Sparks <adamsparks92@gmail.com>
2022-07-28 22:19:25 -0500

    minimal fix (#66)

--- playground/svelte.config.js
+++ playground/svelte.config.js
@@ -14,19 +14,6 @@ const config = {
 
   kit: {
     adapter: adapter(),
-    vite: {
-      resolve: {
-        alias: {
-          flatbread: resolve('../packages/flatbread/src/index.ts'),
-          '@flatbread/transformer-markdown': resolve(
-            '../packages/transformer-markdown/src/index.ts'
-          ),
-          '@flatbread/source-filesystem': resolve(
-            '../packages/source-filesystem/src/index.ts'
-          ),
-        },
-      },
-    },
   },
 
   preprocess: [preprocess({})],
