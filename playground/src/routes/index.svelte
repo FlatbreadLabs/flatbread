8c3c1acf71faf174fa689d55390ff6162edb15a5
tonyketcham <tony@karmalies.studio>
2022-07-31 02:17:58 -0500

    Dynamic fields

--- playground/src/routes/index.svelte
+++ playground/src/routes/index.svelte
@@ -23,7 +23,13 @@
             name
             entity
             enjoys
-            image
+            image {
+              srcset
+              srcsetwebp
+              srcsetavif
+              placeholder
+              aspectratio
+            }
             friend {
               name
               date_joined
@@ -93,9 +99,7 @@
 
 <div class="grid grid-cols-2 divide-x-2 divide-black">
   <Pane label="JSON Output">
-    <pre
-      class="overflow-auto p-3"
-      style="height: calc(100vh - 3.5rem);">
+    <pre class="overflow-auto p-3" style="height: calc(100vh - 3.5rem);">
       <code class="text-sm">
         {JSON.stringify(data, null, 2)}
       </code>
