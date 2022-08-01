ad0a1e7e2565fe39ea26ddad4ccefbecc8a454e5
tonyketcham <tony@karmalies.studio>
2022-07-31 21:05:33 -0500

    static SvelteKit example

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -1,22 +1,17 @@
 import preprocess from 'svelte-preprocess';
-// import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
-import { resolve } from 'path';
-// import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte'],
-
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  // preprocess: [preprocess()],
+  preprocess: [preprocess({})],
 
   kit: {
     adapter: adapter(),
+    prerender: {
+      default: true,
+    },
   },
-
-  preprocess: [preprocess({})],
 };
 
 export default config;
