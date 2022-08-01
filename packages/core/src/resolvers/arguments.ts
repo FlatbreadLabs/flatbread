59096c38c0744bedf5b44b8f156df7d0047e39ea
tonyketcham <tony@karmalies.studio>
2022-08-01 00:58:15 -0500

    add docs and extract `buildFilterQueryFragment`

--- packages/core/src/resolvers/arguments.ts
+++ packages/core/src/resolvers/arguments.ts
@@ -1,5 +1,8 @@
 import { keyBy } from 'lodash-es';
-import sift, { generateFilterSetManifest } from '../utils/sift';
+import sift, {
+  generateFilterSetManifest,
+  TargetAndComparator,
+} from '../utils/sift';
 import { ContentNode, FlatbreadConfig } from '../types';
 import { FlatbreadProvider } from '../providers/base';
 interface ResolveQueryArgsOptions {
@@ -24,6 +27,7 @@ const resolveQueryArgs = async (
   if (filter) {
     // Place the nodes into a keyed object by ID so we can easily filter by ID without doing tons of looping.
     // TODO: store all nodes in an ID-keyed object.
+    // TODO: replace id field with user-defined/fallback identifier field.
     const nodeById = keyBy(nodes, 'id');
 
     // Turn the filter into a GraphQL subquery that returns an array of matching content node IDs.
@@ -46,6 +50,68 @@ const resolveQueryArgs = async (
 };
 
 /**
+ * Builds a GraphQL query fragment from a `filterSetManifest`.
+ *
+ * @example
+ * // Consider this query with a complex filter:
+ *  query AllPosts {
+ *    allPosts(filter: {_content: {timeToRead: {gte: 0}}, title: {wildcard: "Test*"}}) {
+ *      id
+ *      title
+ *    }
+ *  }
+ *
+ * // The filter is converted to:
+ *  filterSetManifest = [
+ *    {
+ *      path: [ '_content', 'timeToRead' ],
+ *      comparator: { operation: 'gte', value: 0 }
+ *    },
+ *    {
+ *      path: [ 'title' ],
+ *      comparator: { operation: 'wildcard', value: 'Test*' }
+ *    }
+ *  ]
+ *
+ * // ...which is passed into this function to then become this fragment:
+ *  _content {
+ *    timeToRead
+ *  }
+ *  title
+ *
+ */
+function buildFilterQueryFragment(filterSetManifest: TargetAndComparator) {
+  let filterToQuery = [];
+
+  for (const filter of filterSetManifest) {
+    let graphQLFieldAccessor = '';
+
+    for (let i = 0; i < filter.path.length; i++) {
+      const field = filter.path[i];
+      const lastFieldIndex = filter.path.length - 1;
+
+      // Build a partial GraphQL query's field shape
+      if (i === lastFieldIndex && filter.path.length === 1) {
+        // If the filter path is a leaf-field, just add the field name.
+        graphQLFieldAccessor += `${field}`;
+      } else if (i !== lastFieldIndex) {
+        // If the filter is not a leaf-field, we need to add the field name and open it to contain child fields.
+        graphQLFieldAccessor += `${field} {`;
+      } else {
+        // If the current field is the last leaf-field of a nested accessor, we need to add the field name and close it with the same number of opening brackets it took to reach this depth.
+        graphQLFieldAccessor += `${field} ${[lastFieldIndex]
+          .map(() => '}')
+          .join('')}`;
+      }
+    }
+
+    filterToQuery.push(graphQLFieldAccessor);
+  }
+
+  return filterToQuery.join('\n');
+}
+
+/**
  * Deeply resolves a filter argument as a subquery, and returns a set of content node IDs that satisfy the filter.
  *
  * @param filter the filter argument
@@ -63,36 +129,15 @@ export const resolveFilter = async (
   // Run Flatbread as a function to execute a subquery
   const flatbread = new FlatbreadProvider(config);
 
-  let filterToQuery = [];
-
-  for (const filter of filterSetManifest) {
-    let graphQLFieldAccessor = '';
-
-    for (let i = 0; i < filter.path.length; i++) {
-      const field = filter.path[i];
-      const lastFieldIndex = filter.path.length - 1;
-
-      // Build a partial GraphQL query's field shape
-      if (i === lastFieldIndex && filter.path.length === 1) {
-        graphQLFieldAccessor += `${field}`;
-      } else if (i !== lastFieldIndex) {
-        graphQLFieldAccessor += `${field} {`;
-      } else {
-        graphQLFieldAccessor += `${field} ${[lastFieldIndex]
-          .map(() => '}')
-          .join('')}`;
-      }
-    }
-
-    filterToQuery.push(graphQLFieldAccessor);
-  }
+  // Build a GraphQL query fragment that will be used to resolve content nodes in a structure expected by the sift function, for the given filter.
+  const filterQueryFragment = buildFilterQueryFragment(filterSetManifest);
 
   // TODO: replace id field with user-defined/fallback identifier field
   const queryString = `
     query ${options.type.pluralQueryName}_FilterSubquery {
       ${options.type.pluralQueryName} {
         id
-        ${filterToQuery.join('\n')}
+        ${filterQueryFragment}
       }
     }
   `;
