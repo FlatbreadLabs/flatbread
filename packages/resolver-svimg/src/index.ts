9c4210c7ef16b5a255e4dc94ed0e23181276b066
Adam Sparks <adamsparks92@gmail.com>
2022-07-31 19:00:36 -0500

    add descriptions to svimg types

--- packages/resolver-svimg/src/index.ts
+++ packages/resolver-svimg/src/index.ts
@@ -5,11 +5,21 @@ const queue = new Queue();
 type Config = Parameters<typeof generateComponentAttributes>[0];
 
 const SVIMG_TYPE = `
+"""An Image Optimized by Svimg"""
 type Svimg {
+  """Responsive images and widths"""
   srcset: String
+
+  """Responsive WebP images and widths -- can be disabled with webp: false in config"""
   srcsetwebp: String
+
+  """Responsive Avif images and widths -- can be disabled with avif: false in config"""
   srcsetavif: String
+
+  """inline blurred placeholder image -- can be disabled with skipPlaceholder: true in config"""
   placeholder: String
+
+  """Aspect ratio of image"""
   aspectratio: Float
 }`;
 
