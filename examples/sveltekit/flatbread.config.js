a5aa89d1418bc0e814a9fb3415802a220f177cbd
Tony Ketcham <tony@modyfi.io>
2022-08-04 01:48:35 -0500

    fix path

--- examples/sveltekit/flatbread.config.js
+++ examples/sveltekit/flatbread.config.js
@@ -48,7 +48,7 @@ export default defineConfig({
         createSvImgField('image', {
           inputDir: 'static/authorImages',
           outputDir: 'static/g',
-          srcGenerator: (path) => '/g' + path,
+          srcGenerator: (path) => '/g/' + path,
         }),
       ],
     },
