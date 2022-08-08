f31e91fca08b9bf36736aa7aecc4ed9153f765de
Adam Sparks <adamsparks92@gmail.com>
2022-08-07 20:06:05 -0500

    remove nanoid-dictionary

--- packages/core/src/utils/createUniqueId.ts
+++ packages/core/src/utils/createUniqueId.ts
@@ -1,9 +1,8 @@
 import { customAlphabet } from 'nanoid';
-import { lowercase, numbers } from 'nanoid-dictionary';
 
-const nanoid = customAlphabet(lowercase + numbers);
+// only use lower case letters and numbers to avoid issues with windows ignoring case on filenames
+const nanoid = customAlphabet('abcdefghijklmnopqrstuvwxyz0123456789');
 
 export function createUniqueId() {
-  // only use lower case to avoid issues with windows ignoring case on filenames
   return nanoid();
 }
