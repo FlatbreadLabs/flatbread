372cf0a2f410e6150807acffa04db88c61096431
Tony Ketcham <tony@modyfi.io>
2025-08-10 02:53:21 -0700

    update test snapshots

--- ava.config.js
+++ ava.config.js
@@ -1,5 +1,9 @@
 export default {
-  files: ['packages/**/*.test.(j|t)s'],
+  files: [
+    'packages/**/*.test.(j|t)s',
+    // Exclude Vitest suites located under __tests__ so AVA doesn't try to run them
+    '!packages/**/src/__tests__/**',
+  ],
   extensions: {
     js: true,
     ts: 'module',
